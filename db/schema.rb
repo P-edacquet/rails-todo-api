c6cb75991f3745ea66f80a0c6dbd67d63be67866
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2021-12-02 12:06:21 +0100

    link between Todo and User done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_082332) do
+ActiveRecord::Schema.define(version: 2021_12_02_101549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_09_28_082332) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_09_28_082332) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "todos", "users"
 end
